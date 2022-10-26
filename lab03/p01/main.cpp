4f7d3b9925ca475fe9963915af0d20b8381b2553
Kobiljon Muhammadov <muhammadov_q@auca.kg>
2022-10-26 12:36:37 +0200

    lab03 p01 fixed!

--- lab03/p01/main.cpp
+++ lab03/p01/main.cpp
@@ -5,8 +5,6 @@ int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
-#include "class.hpp"
-
 vector<vector<int>> readShuffles();
 vector<int> createDeck();
 void applyShuffleToDeck(vector<int> &deck, const vector<int> &shuffle);
@@ -35,4 +33,49 @@ int main()
 
         printDeck(deck);
     }
+}
+
+vector<int> createDeck()
+{
+    vector<int> result(52);
+
+    for (int i = 0; i < 52; ++i)
+        result[i] = i;
+
+    return result;
+}
+
+void applyShuffleToDeck(vector<int> &deck, const vector<int> &shuffle)
+{
+    vector<int> temp(52);
+
+    for (int i = 0; i < 52; ++i)
+        temp[i] = deck[shuffle[i] - 1];
+
+    deck = temp;
+}
+
+void printDeck(const vector<int> &deck)
+{
+    static vector<string> ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+    static vector<string> suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+    for (auto card : deck)
+        cout << ranks[card % 13] << " of " << suits[card / 13] << endl;
+}
+
+vector<vector<int>> readShuffles()
+{
+    int nShuffles;
+    cin >> nShuffles;
+
+    vector<vector<int>> result(nShuffles, vector<int>(52));
+
+    for (int i = 0; i < nShuffles; ++i)
+        for (int j = 0; j < 52; ++j)
+            cin >> result[i][j];
+
+    cin.ignore(1024, '\n');
+
+    return result;
 }
