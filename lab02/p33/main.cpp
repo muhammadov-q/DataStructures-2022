2616328683cdd9177c6306d95ab886f0e5371bd4
Kobiljon Muhammadov <muhammadov_q@auca.kg>
2022-10-28 15:41:59 +0200

    lab02 p33 wrong answer

---
+++ lab02/p33/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+    int t, n;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        vector<int> st(n);
+        double sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> st[i];
+            sum += st[i];
+        }
+
+        double avarage = sum / n;
+        int cnt = 0;
+
+        for(int s : st)
+        {
+            if (s > avarage)
+            {
+                cnt++;
+            }
+        }
+
+        cout << fixed << setprecision(3) << (cnt*100)/n << "%\n";
+    }
+}
