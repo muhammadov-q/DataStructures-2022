2065d586ffefcdc265bb355001d53532e13cc50e
Kobiljon Muhammadov <muhammadov_q@auca.kg>
2022-10-31 15:26:39 +0100

    lab02 p19 accepted with new version!

--- lab02/p19/main.cpp
+++ lab02/p19/main.cpp
@@ -12,35 +12,15 @@ int main()
     string str; getline(cin, str);
     while (T--) {
         getline(cin, str);
-        istringstream sinp(str);
-        int m = 0, f = 0, mf = 0, pieces = 0;
-        bool isLoop = false;
-        while (sinp.good()) {
-            pieces++;
-            sinp >> str;
-            if (!str.compare("MM")) 
-            {
-                m += 2;
-            }
-            else if (!str.compare("FF")) 
-            {
-                f += 2;
-            }
-            else {
-                mf++;
-            }
-        } if (pieces > 1) 
+        int male = count(str.begin(), str.end(), 'M');
+        int female = count(str.begin(), str.end(), 'F');
+        if (male == female && (male > 1 && female > 1))
         {
-            if (mf > 1) 
-            {
-                m++;
-                f++;
-            }
-            if (m == f) 
-            {
-                isLoop = true;
-            }
+            cout << "LOOP" << endl;
         }
-        cout << (isLoop ? "LOOP" : "NO LOOP") << endl;
+        else
+        {
+            cout << "NO LOOP" << endl;
+        }
     }
 }
