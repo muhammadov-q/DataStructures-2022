938e9a9c5816749479e89ab08283784a1296ff8f
Kobiljon Muhammadov <muhammadov_q@auca.kg>
2022-12-02 13:49:16 +0100

    p03, p0201 and p0202 completed!

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -83,6 +83,119 @@ void p0102()
     }
 }
 
+void p0201()
+{
+    //vector<int> v = {3, 1, 20, 4, 7, 0, 5};
+
+    //IsEven functor;
+    //auto itV = find_if(begin(v), end(v), IsEven());
+    //if (itV != end(v))
+    //{
+    //    cout << "position of the first even number is " << itV - begin(v) << endl;
+    //    cout << "the number is " << *itV << endl;
+    //}
+    //else
+    //{
+    //    cout << "even numbers not found" << endl;
+    //}
+
+    vector<int> v2 = {3, 1, 20, 4, 7, 0, 5};
+
+    //lambda expression;
+    auto itV2 = find_if(begin(v2), end(v2), [](int n)
+                        { return n % 2 == 0; });
+
+    if (itV2 != end(v2))
+    {
+        cout << "position of the first even number is " << itV2 - begin(v2) << endl;
+        cout << "the number is " << *itV2 << endl;
+    }
+    else
+    {
+        cout << "even numbers not found" << endl;
+    }
+}
+
+void p0202()
+{
+    vector<int> v = {3, 1, 20, 4, 7, 0, 5};
+
+    //IsEven functor;
+    auto it = auFindIf(begin(v), end(v), IsEven());
+    if (it != end(v))
+    {
+        cout << "position of the first even number is " << it - begin(v) << endl;
+        cout << "the number is " << *it << endl;
+    }
+    else
+    {
+        cout << "even numbers not found" << endl;
+    }
+
+    vector<int> v2 = {3, 1, 20, 4, 7, 0, 5};
+
+    //lambda expression;
+    auto itV2 = auFindIf(begin(v2), end(v2), [](int value)
+                         { return value % 2 == 0; });
+
+    if (itV2 != end(v2))
+    {
+        cout << "position of the first even number is " << itV2 - begin(v2) << endl;
+        cout << "the number is " << *itV2 << endl;
+    }
+    else
+    {
+        cout << "even numbers not found" << endl;
+    }
+}
+
+struct GreaterThan
+{
+    int mX;
+    GreaterThan(int x)
+        : mX(x)
+    {
+    }
+
+    bool operator()(int n) const
+    {
+        return n > mX;
+    }
+};
+
+
+void p03()
+{
+    vector<int> v = {3, 1, 20, 4, 7, 0, 5};
+
+    cout << "Enter x: ";
+    int x;
+    cin >> x;
+
+    auto it = find_if(begin(v), end(v), 
+                        [x](int n) 
+                        { return n > x; });
+
+    if (it != end(v))
+    {
+        cout << *it << " found. Its index is " << it - begin(v) << endl;
+    }
+    else
+    {
+        cout << "even numbers not found" << endl;
+    }
+
+    auto it2 = find_if(begin(v), end(v), GreaterThan(x));
+    if (it2 != end(v))
+    {
+        cout << *it2 << " found. Its index is " << it2 - begin(v) << endl;
+    }
+    else
+    {
+        cout << "even numbers not found" << endl;
+    }
+}
+
 void p06()
 {
     vector<Student> students;
@@ -117,9 +230,14 @@ void p06()
 
 int main()
 {
-    //p0101();
-    //p0102();
+    // p0101();
+    // p0102();
 
-    p06();
+    // p0201();
+    // p0202();
+
+    // p06();
+
+    p03();
 
 }
