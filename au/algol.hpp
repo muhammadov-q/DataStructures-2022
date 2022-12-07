f78ff9727b19a56432f41ecdb60da17c553ecf92
Kobiljon Muhammadov <muhammadov_q@auca.kg>
2022-12-07 16:45:03 +0100

    au completed!

--- au/algol.hpp
+++ au/algol.hpp
@@ -55,3 +55,66 @@ ForwardIter auFindIf(ForwardIter beg, ForwardIter end, UnaryPredicate pred)
 
     return beg;
 }
+
+
+template <typename ForwardIt>
+ForwardIt auMinElement(ForwardIt beg, ForwardIt end)
+{
+    ForwardIt min = beg;
+    while (beg != end)
+    {
+        if (*min > *beg)
+        {
+            min = beg;
+        }
+
+        beg++;
+    }
+
+    return min;
+}
+
+template <typename ForwardIt, typename Compare>
+ForwardIt auMinElement(ForwardIt beg, ForwardIt end, Compare comp)
+{
+    ForwardIt min = beg;
+    while (beg != end)
+    {
+        if (comp(*beg, *min))
+        {
+            min = beg;
+        }
+
+        beg++;
+    }
+
+    return min;
+}
+
+template <class ForwardIt, class T>
+bool auBinarySearch(ForwardIt beg, ForwardIt end, const T &value)
+{
+    while (beg != end)
+    {
+        ForwardIt middle = beg;
+        advance(middle, (end - beg) / 2);
+
+        if (value > *middle)
+        {
+            beg = middle;
+        }
+        else if (value < *middle)
+        {
+            end = middle;
+        }
+        else
+        {
+            return true;
+        }
+
+        if (end - beg == 1 && (value != *beg && value != *end))
+            break;
+    }
+
+    return false;
+}
