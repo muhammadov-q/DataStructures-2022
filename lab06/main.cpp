d1194ac4895c20535279a77823ae731819a0d04c
Kobiljon Muhammadov <muhammadov_q@auca.kg>
2022-12-08 22:37:17 +0100

    p12 from lab06 finished!

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -520,6 +520,19 @@ void p11()
     }
 }
 
+void p12()
+{
+    vector<int> v = {0, 0, 4, 5, 10, 10, 10, 12, 20, 25, 35, 25, 25, 40};
+
+    sort(begin(v), end(v));
+
+    for (int x; cin >> x;)
+    {
+        auto it = equal_range(begin(v), end(v), x);
+        cout << "number of occurences of " << x << " is " << it.second - it.first << endl;
+    }
+}
+
 int main()
 {
     // p0101();
@@ -547,5 +560,7 @@ int main()
     //p1001();
     //p1002();
 
-    p11();
+    //p11();
+
+    p12();
 }
