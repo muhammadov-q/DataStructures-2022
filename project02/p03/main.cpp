49b8507eca61253e3fc11fac139c54eb46bc33eb
Kobiljon Muhammadov <muhammadov_q@auca.kg>
2022-12-11 12:06:55 +0100

    p03 integrated version accepted!

--- project02/p03/main.cpp
+++ project02/p03/main.cpp
@@ -1,84 +1,78 @@
 #include <bits/stdc++.h>
 
 template <typename C>
-int sz(const C &col) { return static_cast<int>(col.size()); }
+int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
 struct Freq
 {
-    char letter;
-    int count;
+    char mLetter;
+    int mCount;
+
+    Freq(char letter, int count)
+        : mLetter(letter), mCount(count)
+    {
+    }
 };
 
-Freq make_freq(char letter, int count)
+bool operator==(Freq const &a, char const &b)
 {
-    Freq a;
-    a.letter = letter;
-    a.count = count;
-
-    return a;
+    return a.mLetter == b;
 }
 
 int main()
 {
     iostream::sync_with_stdio(false);
+    int nTests;
+    cin >> nTests;
 
-    int tc;
-    cin >> tc;
-    for (int test = 1; test <= tc; test++)
+    for (int test = 1; test <= nTests; test++)
     {
-        int row, col, impBytes, nonImpBytes;
-        cin >> row >> col >> impBytes >> nonImpBytes;
+        int col, row, import, nonImport;
+        cin >> col >> row >> import >> nonImport;
+        vector<Freq> frequences;
 
-        vector<Freq> vc;
-        char letter;
-        for (int i = 0; i < row; i++)
+        for (int i = 0; i < col; i++)
         {
-            for (int j = 0; j < col; j++)
-            {
-                cin >> letter;
-                auto it = lower_bound(vc.begin(), vc.end(), letter, [](Freq a, char w)
-                                      { return a.letter < w; });
+            string line;
+            cin >> line;
 
-                if (it != vc.end())
+            for (int j = 0; j < row; j++)
+            {
+                auto it = find(begin(frequences), end(frequences), line[j]);
+                if (it == end(frequences))
                 {
-                    if (it->letter == letter)
-                    {
-                        it->count++;
-                    }
-                    else
-                    {
-                        Freq a = make_freq(letter, 1);
-                        vc.insert(it, a);
-                    }
+                    frequences.push_back(Freq(line[j], 1));
                 }
                 else
                 {
-                    Freq a = make_freq(letter, 1);
-                    vc.push_back(a);
+                    it->mCount++;
                 }
             }
         }
 
-        sort(vc.begin(), vc.end(), [](Freq a, Freq b)
-                { return a.count > b.count; });
-
-        int freq = (*(vc.begin())).count;
-
-        for (vector<Freq>::iterator a = vc.begin(); a < vc.end() - 1; a++)
-        {
-            if ((*a).count == (*(1 + a)).count)
+        sort(begin(frequences), end(frequences), [](Freq a, Freq b)
             {
-                freq += (*a).count;
+                return a.mCount < b.mCount;
+            });
+
+        int maxVal = (end(frequences) - 1)->mCount;
+
+        int result = 0;
+        auto k = begin(frequences);
+        for (; k != end(frequences); k++)
+        {
+            if (k->mCount == maxVal)
+            {
+                result += ((k->mCount) * import);
             }
             else
             {
-                break;
+                result += ((k->mCount) * nonImport);
             }
         }
-        
-        int nfreq = row * col - freq;
-        cout << "Case " << test << ": " << freq * impBytes + nfreq * nonImpBytes << endl;
+
+        cout << "Case " << test << ": " << result << '\n';
     }
 }
